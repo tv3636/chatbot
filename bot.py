5f4910a586b6f6559835735bd360bed5e36b6cf2
tv3636 <tv3636@gmail.com>
2021-11-20 19:49:47 -0800

    redo

--- bot.py
+++ bot.py
@@ -22,6 +22,8 @@ directory_prefix = "./channel_history_"
 last_n = 10
 
 def replaceTags(message):
+	if '<@' in message:
+		print(client.get_user(int(message[message.find('@') + 1:message.find('>')])))
 	return re.sub(r'<@.*>', tag_replace, message)
 
 def ask(sender, question):
