d9036933ab6e64cbd725d942719273aaecf051d2
tv3636 <tv3636@gmail.com>
2021-11-20 19:56:40 -0800

    last try I swear

--- bot.py
+++ bot.py
@@ -23,10 +23,10 @@ last_n = 10
 
 def replaceTags(message):
 	if '<@!' in message:
-		print(client.get_user(int(message[message.find('!') + 1:message.find('>')])))
+		print(client.fetch_user(int(message[message.find('!') + 1:message.find('>')])))
 	elif '<@' in message:
-		print(client.get_user(int(message[message.find('@') + 1:message.find('>')])))
-		
+		print(client.fetch_user(int(message[message.find('@') + 1:message.find('>')])))
+
 	return re.sub(r'<@.*>', tag_replace, message)
 
 def ask(sender, question):
